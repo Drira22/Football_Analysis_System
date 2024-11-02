09d25d87219b1920bfe5e13047970e31630068d7
Mohamed Khalil Drira <Mohamedkhalil.drira@supcom.tn>
2024-11-02 13:16:09 +0100

    assign ball aquisition

--- trackers/tacker.py
+++ trackers/tacker.py
@@ -189,6 +189,8 @@ class Tracker:
             for track_id,player in player_dict.items():
                 color=player.get("team_color",(0,255,255))
                 frame = self.draw_ellipse(frame,player["bbox"],color,track_id)
+                if player.get('has_ball',False):
+                    frame=self.draw_triangle(frame,player["bbox"],(0,0,255))
             
             #Draw Referee
             for track_id,referee in referees_dict.items():
